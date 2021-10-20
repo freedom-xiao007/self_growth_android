0cc7feb87f66f0fde8e758792cdd4e3c103b7347
萧 <1243925457@qq.com>
2021-10-20 21:19:01 +0800

    将活动监听移动到前台服务中

--- app/src/main/java/com/example/selfgrowth/MainActivity.java
+++ app/src/main/java/com/example/selfgrowth/MainActivity.java
@@ -1,17 +1,11 @@
 package com.example.selfgrowth;
 
-import android.app.usage.UsageEvents;
-import android.app.usage.UsageStatsManager;
-import android.content.Context;
 import android.content.Intent;
 import android.os.Build;
 import android.os.Bundle;
-import android.os.Handler;
 import android.provider.Settings;
-import android.util.Log;
 import android.view.Menu;
 
-import com.example.selfgrowth.http.PhoneUseRecord;
 import com.example.selfgrowth.scheduler.MonitorTask;
 import com.example.selfgrowth.server.forground.MyForeGroundService;
 import com.example.selfgrowth.service.backgroud.MonitorService;
@@ -32,17 +26,11 @@ import com.example.selfgrowth.databinding.ActivityMainBinding;
 
 import java.util.concurrent.TimeUnit;
 
-import retrofit2.Call;
-import retrofit2.Callback;
-import retrofit2.Response;
-import retrofit2.Retrofit;
-import retrofit2.converter.gson.GsonConverterFactory;
-
 public class MainActivity extends AppCompatActivity {
 
     private AppBarConfiguration mAppBarConfiguration;
     private ActivityMainBinding binding;
-    private String beforeActivity;
+
 
     @RequiresApi(api = Build.VERSION_CODES.O)
     @Override
@@ -83,79 +71,9 @@ public class MainActivity extends AppCompatActivity {
 
         PeriodicWorkRequest monitorTask = new PeriodicWorkRequest.Builder(MonitorTask.class, 3, TimeUnit.SECONDS).build();
         WorkManager.getInstance(this).enqueue(monitorTask);
-
-        Handler handler=new Handler();
-        Runnable runnable=new Runnable(){
-            @Override
-            public void run() {
-// TODO Auto-generated method stub
-//要做的事情
-                Log.d("Monitor Detect", "定时检测顶层应用");
-                getTopActivity();
-                handler.postDelayed(this, 10000);
-            }
-        };
-        handler.postDelayed(runnable, 10000);//每两秒执行一次runnable.
     }
 
-    public void getTopActivity()
-    {
-        long endTime = System.currentTimeMillis();
-        long beginTime = endTime - 10000;
-        UsageStatsManager sUsageStatsManager = (UsageStatsManager) this.getSystemService(Context.USAGE_STATS_SERVICE);
-        String result = "";
-        UsageEvents.Event event = new UsageEvents.Event();
-        UsageEvents usageEvents = sUsageStatsManager.queryEvents(beginTime, endTime);
-        while (usageEvents.hasNextEvent()) {
-            usageEvents.getNextEvent(event);
-            if (event.getEventType() == UsageEvents.Event.MOVE_TO_FOREGROUND) {
-                result = event.getPackageName()+"/"+event.getClassName();
-            }
-        }
-        if (!android.text.TextUtils.isEmpty(result)) {
-            Log.i("Service", result);
-            sendRecord(result);
-            beforeActivity = result;
-        } else {
-            sendRecord(beforeActivity);
-        }
-    }
 
-    private void sendRecord(String topActivity) {
-        if (topActivity == null) {
-            Snackbar.make(getWindow().getDecorView(), "activity is null", Snackbar.LENGTH_LONG)
-                    .setAction("Action", null).show();
-            return;
-        }
-
-        //创建Retrofit对象
-        Retrofit retrofit = new Retrofit.Builder()
-                .baseUrl("http://192.168.1.3:8080/") //基础url,其他部分在GetRequestInterface里
-                .addConverterFactory(GsonConverterFactory.create()) //Gson数据转换器
-                .build();
-
-        //创建网络请求接口实例
-        PhoneUseRecord request = retrofit.create(PhoneUseRecord.class);
-        Call<String> call = request.uploadRecord(topActivity);
-
-        //发送网络请求(异步)
-        call.enqueue(new Callback<String>() {
-            @Override
-            public void onResponse(Call<String> call, Response<String> response) {
-                String res = response.body();
-                Log.d("http response", res);
-                Snackbar.make(getWindow().getDecorView(), "upload:" + topActivity, Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-
-            @Override
-            public void onFailure(Call<String> call, Throwable t) {
-                System.out.println("GetOutWarehouseList->onFailure(MainActivity.java): "+t.toString() );
-                Snackbar.make(getWindow().getDecorView(), t.toString(), Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
-    }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
--- app/src/main/java/com/example/selfgrowth/server/forground/MyForeGroundService.java
+++ app/src/main/java/com/example/selfgrowth/server/forground/MyForeGroundService.java
@@ -1,25 +1,33 @@
 package com.example.selfgrowth.server.forground;
 
 import android.annotation.SuppressLint;
-import android.annotation.TargetApi;
-import android.app.Notification;
 import android.app.NotificationChannel;
 import android.app.NotificationManager;
-import android.app.PendingIntent;
 import android.app.Service;
+import android.app.usage.UsageEvents;
+import android.app.usage.UsageStatsManager;
 import android.content.Context;
 import android.content.Intent;
 import android.os.Build;
+import android.os.Handler;
 import android.os.IBinder;
 import android.util.Log;
 
 import androidx.annotation.Nullable;
 import androidx.core.app.NotificationCompat;
 
-import com.example.selfgrowth.MainActivity;
 import com.example.selfgrowth.R;
+import com.example.selfgrowth.http.PhoneUseRecord;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
 
 public class MyForeGroundService extends Service {
+
+    private String beforeActivity;
 
     /*
      * @param intent
@@ -66,8 +74,8 @@ public class MyForeGroundService extends Service {
         //在创建的通知渠道上发送通知
         NotificationCompat.Builder builder = new NotificationCompat.Builder(this, "232");
         builder.setSmallIcon(R.drawable.ic_launcher_foreground) //设置通知图标
-                .setContentTitle("前台服务通知")//设置通知标题
-                .setContentText("前台服务通知")//设置通知内容
+                .setContentTitle("正在监控手机活动并上报")//设置通知标题
+                .setContentText("正在监控手机活动并上报")//设置通知内容
                 .setAutoCancel(true) //用户触摸时，自动关闭
                 .setOngoing(true);//设置处于运行状态
         //向系统注册通知渠道，注册后不能改变重要性以及其他通知行为
@@ -75,5 +83,71 @@ public class MyForeGroundService extends Service {
         notificationManager.createNotificationChannel(channel);
         //将服务置于启动状态 NOTIFICATION_ID指的是创建的通知的ID
         startForeground(232,builder.build());
+
+        Handler handler=new Handler();
+        Runnable runnable=new Runnable(){
+            @Override
+            public void run() {
+// TODO Auto-generated method stub
+//要做的事情
+                Log.d("Monitor Detect", "定时检测顶层应用");
+                getTopActivity();
+                handler.postDelayed(this, 10000);
+            }
+        };
+        handler.postDelayed(runnable, 10000);//每两秒执行一次runnable.
+    }
+
+    public void getTopActivity()
+    {
+        long endTime = System.currentTimeMillis();
+        long beginTime = endTime - 10000;
+        UsageStatsManager sUsageStatsManager = (UsageStatsManager) this.getSystemService(Context.USAGE_STATS_SERVICE);
+        String result = "";
+        UsageEvents.Event event = new UsageEvents.Event();
+        UsageEvents usageEvents = sUsageStatsManager.queryEvents(beginTime, endTime);
+        while (usageEvents.hasNextEvent()) {
+            usageEvents.getNextEvent(event);
+            if (event.getEventType() == UsageEvents.Event.MOVE_TO_FOREGROUND) {
+                result = event.getPackageName()+"/"+event.getClassName();
+            }
+        }
+        if (!android.text.TextUtils.isEmpty(result)) {
+            Log.i("Service", result);
+            sendRecord(result);
+            beforeActivity = result;
+        } else {
+            sendRecord(beforeActivity);
+        }
+    }
+
+    private void sendRecord(String topActivity) {
+        if (topActivity == null) {
+            return;
+        }
+
+        //创建Retrofit对象
+        Retrofit retrofit = new Retrofit.Builder()
+                .baseUrl("http://192.168.1.3:8080/") //基础url,其他部分在GetRequestInterface里
+                .addConverterFactory(GsonConverterFactory.create()) //Gson数据转换器
+                .build();
+
+        //创建网络请求接口实例
+        PhoneUseRecord request = retrofit.create(PhoneUseRecord.class);
+        Call<String> call = request.uploadRecord(topActivity);
+
+        //发送网络请求(异步)
+        call.enqueue(new Callback<String>() {
+            @Override
+            public void onResponse(Call<String> call, Response<String> response) {
+                String res = response.body();
+                Log.d("http response", res);
+            }
+
+            @Override
+            public void onFailure(Call<String> call, Throwable t) {
+                System.out.println("GetOutWarehouseList->onFailure(MainActivity.java): "+t.toString() );
+            }
+        });
     }
 }
