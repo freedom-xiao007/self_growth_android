ae2fb5f2f2ea6d1568f71b59006b0508d56284cb
萧 <1243925457@qq.com>
2021-10-20 21:26:21 +0800

    Update MainActivity.java

--- app/src/main/java/com/example/selfgrowth/MainActivity.java
+++ app/src/main/java/com/example/selfgrowth/MainActivity.java
@@ -1,7 +1,6 @@
 package com.example.selfgrowth;
 
 import android.app.AppOpsManager;
-import android.content.Context;
 import android.content.Intent;
 import android.content.pm.ApplicationInfo;
 import android.content.pm.PackageManager;
