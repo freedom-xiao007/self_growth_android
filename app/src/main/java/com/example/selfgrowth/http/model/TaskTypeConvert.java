d286d40f098423d5f21184e72b50f7c4f9625250
萧 <1243925457@qq.com>
2021-10-23 18:11:59 +0800

    Update TaskTypeConvert.java

--- app/src/main/java/com/example/selfgrowth/http/model/TaskTypeConvert.java
+++ app/src/main/java/com/example/selfgrowth/http/model/TaskTypeConvert.java
@@ -8,9 +8,10 @@ import lombok.SneakyThrows;
 
 public class TaskTypeConvert {
 
-    public static final Map<String, Integer> convert = init();
+    public static final Map<String, Integer> convertKey = initKey();
+    public static final Map<Integer, String> convertValue = initValue();
 
-    private static Map<String, Integer> init() {
+    private static Map<String, Integer> initKey() {
         return ImmutableMap.<String, Integer>builder()
                 .put("输入", 0)
                 .put("输出", 1)
@@ -18,11 +19,27 @@ public class TaskTypeConvert {
                 .build();
     }
 
+    private static Map<Integer, String> initValue() {
+        return ImmutableMap.<Integer, String>builder()
+                .put(0, "输入")
+                .put(1, "输出")
+                .put(2, "日常")
+                .build();
+    }
+
     @SneakyThrows
-    public static Integer convert(String key) {
-        if (!convert.containsKey(key)) {
+    public static Integer convertToValue(String key) {
+        if (!convertKey.containsKey(key)) {
             throw new Exception("没有该映射");
         }
-        return convert.get(key);
+        return convertKey.get(key);
+    }
+
+    @SneakyThrows
+    public static String convertToKey(Integer value) {
+        if (!convertValue.containsKey(value)) {
+            throw new Exception("没有该映射");
+        }
+        return convertValue.get(value);
     }
 }
