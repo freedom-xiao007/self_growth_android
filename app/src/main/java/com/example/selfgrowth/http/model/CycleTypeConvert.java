b4b5ddb86bc2d1bc79e4634a0e1172a2c75870b3
萧 <1243925457@qq.com>
2021-10-23 18:11:54 +0800

    Update CycleTypeConvert.java

--- app/src/main/java/com/example/selfgrowth/http/model/CycleTypeConvert.java
+++ app/src/main/java/com/example/selfgrowth/http/model/CycleTypeConvert.java
@@ -1,7 +1,6 @@
 package com.example.selfgrowth.http.model;
 
 import com.google.common.collect.ImmutableMap;
-import com.google.common.collect.ImmutableTable;
 
 import java.util.Map;
 
@@ -12,9 +11,10 @@ import lombok.SneakyThrows;
  */
 public class CycleTypeConvert {
 
-    private final static Map<String, Integer> convert = init();
+    private final static Map<String, Integer> convertKey = initKey();
+    private final static Map<Integer, String> convertValue = initValue();
 
-    private static Map<String, Integer> init() {
+    private static Map<String, Integer> initKey() {
         return ImmutableMap.<String, Integer>builder()
                 .put("单次", 0)
                 .put("每日", 1)
@@ -24,11 +24,29 @@ public class CycleTypeConvert {
                 .build();
     }
 
+    private static Map<Integer, String> initValue() {
+        return ImmutableMap.<Integer, String>builder()
+                .put(0, "单次")
+                .put(1, "每日")
+                .put(2, "每周")
+                .put(3, "每月")
+                .put(4, "每年")
+                .build();
+    }
+
     @SneakyThrows
-    public static Integer convert(String key) {
-        if (!convert.containsKey(key)) {
+    public static Integer convertToValue(String key) {
+        if (!convertKey.containsKey(key)) {
             throw new Exception("没有该映射");
         }
-        return convert.get(key);
+        return convertKey.get(key);
+    }
+
+    @SneakyThrows
+    public static String convertToKey(Integer value) {
+        if (!convertValue.containsKey(value)) {
+            throw new Exception("没有该映射");
+        }
+        return convertValue.get(value);
     }
 }
