a969422645e621aed7a88c1524ab9e6af4ecaa56
萧 <1243925457@qq.com>
2021-10-23 14:36:47 +0800

    添加登录导航入口

--- app/src/main/java/com/example/selfgrowth/MainActivity.java
+++ app/src/main/java/com/example/selfgrowth/MainActivity.java
@@ -53,7 +53,8 @@ public class MainActivity extends AppCompatActivity {
         // Passing each menu ID as a set of Ids because each
         // menu should be considered as top level destinations.
         mAppBarConfiguration = new AppBarConfiguration.Builder(
-                R.id.nav_login, R.id.nav_home, R.id.nav_learn, R.id.nav_running, R.id.nav_sleep, R.id.nav_work, R.id.nav_add_task)
+                R.id.nav_login, R.id.nav_home, R.id.nav_learn, R.id.nav_running, R.id.nav_sleep,
+                R.id.nav_work, R.id.nav_add_task, R.id.nav_login)
                 .setOpenableLayout(drawer)
                 .build();
         NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_content_main);
