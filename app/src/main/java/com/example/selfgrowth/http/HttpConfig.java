ce91e4f48b6ddd2b0f37aa810e2b22a95bdf3bb9
萧 <1243925457@qq.com>
2022-02-18 04:27:36 +0800

    Update HttpConfig.java

--- app/src/main/java/com/example/selfgrowth/http/HttpConfig.java
+++ app/src/main/java/com/example/selfgrowth/http/HttpConfig.java
@@ -2,6 +2,5 @@ package com.example.selfgrowth.http;
 
 public class HttpConfig {
 
-    public static final String ADDRESS = "http://192.168.1.3:8080/";
-//    public static final String ADDRESS = "http://selfgrowth.com.cn /";
+    public static final String ADDRESS = "http://localhost:8080/";
 }
