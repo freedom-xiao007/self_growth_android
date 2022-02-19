8ab2fe9a2fae622a91d24c6a2cd79955c0ed7d8e
萧 <1243925457@qq.com>
2022-02-19 17:59:17 +0800

    本地ip调试地址

--- app/src/main/java/com/example/selfgrowth/http/HttpConfig.java
+++ app/src/main/java/com/example/selfgrowth/http/HttpConfig.java
@@ -2,5 +2,5 @@ package com.example.selfgrowth.http;
 
 public class HttpConfig {
 
-    public static final String ADDRESS = "http://localhost:8080/";
+    public static final String ADDRESS = "http://192.168.0.101:8080/";
 }
