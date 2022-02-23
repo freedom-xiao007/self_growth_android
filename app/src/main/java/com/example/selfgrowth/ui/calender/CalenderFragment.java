6ba3203de2323b6392dc4db000622b45e0715ceb
萧 <1243925457@qq.com>
2022-02-24 05:11:26 +0800

    日程初始化

---
+++ app/src/main/java/com/example/selfgrowth/ui/calender/CalenderFragment.java
@@ -0,0 +1,23 @@
+package com.example.selfgrowth.ui.calender;
+
+import android.os.Build;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.RequiresApi;
+import androidx.fragment.app.Fragment;
+
+import com.example.selfgrowth.R;
+
+public class CalenderFragment extends Fragment {
+
+    @RequiresApi(api = Build.VERSION_CODES.O)
+    public View onCreateView(@NonNull LayoutInflater inflater,
+                             ViewGroup container, Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_calender, container, false);
+        return view;
+    }
+}
