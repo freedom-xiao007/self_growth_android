4d41eacf651cfa8a0849c8a5ab05df159d48fe15
萧 <1243925457@qq.com>
2022-04-04 08:42:55 +0800

    增加任务点击详情，调整删除任务按钮

--- app/src/main/java/com/example/selfgrowth/ui/task/ListViewDemoAdapter.java
+++ app/src/main/java/com/example/selfgrowth/ui/task/ListViewDemoAdapter.java
@@ -8,6 +8,7 @@ import android.widget.BaseAdapter;
 import android.widget.CheckBox;
 import android.widget.TextView;
 
+import com.codingending.popuplayout.PopupLayout;
 import com.example.selfgrowth.R;
 import com.example.selfgrowth.model.TaskConfig;
 import com.example.selfgrowth.http.request.TaskRequest;
@@ -18,8 +19,8 @@ import java.util.List;
 public class ListViewDemoAdapter extends BaseAdapter {
 
     private final TaskRequest taskRequest = new TaskRequest();
-    private final Context context;//上下文对象
-    private final List<TaskConfig> dataList;//ListView显示的数据
+    private final Context context;
+    private final List<TaskConfig> dataList;
     private final TaskFragment taskFragment;
     private final String groupName;
     private final TaskService taskService = TaskService.getInstance();
@@ -66,23 +67,29 @@ public class ListViewDemoAdapter extends BaseAdapter {
             viewHolder = (ViewHolder) convertView.getTag();
         }
 
-        boolean isComplete = dataList.get(position).getIsComplete();
-        viewHolder.id.setText(dataList.get(position).getId());
-        viewHolder.name.setText(dataList.get(position).getName());
+        final TaskConfig item = dataList.get(position);
+        boolean isComplete = item.getIsComplete();
+        viewHolder.id.setText(item.getId());
+        viewHolder.name.setText(item.getName());
+        viewHolder.name.setOnClickListener(v -> {
+            View view = View.inflate(context, R.layout.task_detail, null);
+            ((TextView) view.findViewById(R.id.task_label)).setText(item.getLabel().getName());
+            ((TextView) view.findViewById(R.id.task_cycle)).setText(item.getCycleType().getName());
+
+            PopupLayout popupLayout= PopupLayout.init(context, view);
+            view.findViewById(R.id.task_delete).setOnClickListener(v1 -> {
+                taskService.delete(groupName, item.getId());
+                taskFragment.initTaskListOfGroup(groupName);
+                popupLayout.dismiss();
+            });
+            popupLayout.show();
+        });
         viewHolder.status.setText(isComplete ? "已完成" : "未完成");
-//        viewHolder.label.setText(dataList.get(position).getLabel());
-//        viewHolder.cycle.setText(CycleTypeConvert.convertToKey(dataList.get(position).getCycleType()));
-//        viewHolder.type.setText(TaskTypeConvert.convertToKey(dataList.get(position).getType()));
-//        viewHolder.isComplete.setText(dataList.get(position).isComplete() ? "已完成" : "未完成");
         viewHolder.complete.setVisibility(isComplete ? View.GONE : View.VISIBLE);
         viewHolder.complete.setOnClickListener(v -> {
-            taskService.complete(groupName, dataList.get(position).getId());
+            taskService.complete(groupName, item.getId());
             taskFragment.initTaskListOfGroup(groupName);
         });
-//        viewHolder.delete.setOnClickListener(v -> {
-//            taskService.delete(groupName, dataList.get(position).getId());
-//            taskFragment.initTaskListOfGroup(groupName);
-//        });
         return convertView;
     }
 
@@ -93,12 +100,7 @@ public class ListViewDemoAdapter extends BaseAdapter {
         private final TextView id;
         private final TextView name;
         private final TextView status;
-//        private final TextView label;
-//        private final TextView cycle;
-//        private final TextView type;
-//        private final TextView isComplete;
         private final CheckBox complete;
-//        private final CheckBox delete;
 
         /**
          * 构造器
@@ -108,12 +110,7 @@ public class ListViewDemoAdapter extends BaseAdapter {
             id = view.findViewById(R.id.task_id);
             name = view.findViewById(R.id.task_name);
             status = view.findViewById(R.id.task_status);
-//            label = view.findViewById(R.id.task_label);
-//            cycle = view.findViewById(R.id.task_cycle);
-//            type = view.findViewById(R.id.task_type);
-//            isComplete = view.findViewById(R.id.task_isComplete);
             complete = view.findViewById(R.id.task_complete);
-//            delete = view.findViewById(R.id.task_delete);
         }
     }
 }
