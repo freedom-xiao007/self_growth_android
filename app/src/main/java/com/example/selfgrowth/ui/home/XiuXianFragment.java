3e97e51c97872159fa7d297593a9a391e2bb59e3
萧 <1243925457@qq.com>
2022-06-11 08:30:28 +0800

    修仙首页初步

---
+++ app/src/main/java/com/example/selfgrowth/ui/home/XiuXianFragment.java
@@ -0,0 +1,20 @@
+package com.example.selfgrowth.ui.home;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+
+import com.example.selfgrowth.R;
+
+public class XiuXianFragment extends Fragment {
+
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater,
+                             ViewGroup container, Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.xiu_xian, container, false);
+    }
+}
