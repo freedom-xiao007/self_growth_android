13b17d502ed74a640b9a072d37134fbdacab361c
萧 <1243925457@qq.com>
2022-06-12 16:50:56 +0800

    去掉任务和今日数据导航

--- app/src/main/java/com/example/selfgrowth/MainActivity.java
+++ app/src/main/java/com/example/selfgrowth/MainActivity.java
@@ -59,7 +59,7 @@ public class MainActivity extends AppCompatActivity {
         // Passing each menu ID as a set of Ids because each
         // menu should be considered as top level destinations.
         AppBarConfiguration appBarConfiguration = new AppBarConfiguration.Builder(
-                R.id.navigation_home, R.id.navigation_task, R.id.navigation_dashboard, R.id.navigation_calender, R.id.navigation_user)
+                R.id.navigation_home, R.id.navigation_user)
                 .build();
         NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_activity_main);
         NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
